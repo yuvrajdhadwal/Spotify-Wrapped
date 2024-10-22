dc23d947889458c5a4654df846440d8af226bc94
Yixiong Hao <yixiong_hao@outlook.com>
2024-10-21 21:04:05 -0400

    added CORS in setings

--- backend/spotify_data/models.py
+++ backend/spotify_data/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class Song(models.Model):
+class Song(models.Model): #toy model, testing out rest framework
     title = models.CharField(max_length=100)
     runTime = models.IntegerField()
