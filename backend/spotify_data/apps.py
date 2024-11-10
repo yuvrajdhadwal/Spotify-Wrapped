ff09c2f4436e1087b47db6c1f55e4c6cb1deee27
Yuvraj <ydhadwal3@gatech.edu>
2024-11-10 15:14:38 -0500

    lfg

--- backend/spotify_data/apps.py
+++ backend/spotify_data/apps.py
@@ -1,6 +1,8 @@
+'''DOCSTRINGS FOOLS'''
 from django.apps import AppConfig
 
 
 class SpotifyDataConfig(AppConfig):
+    '''DOCSTRINGS FOOLS'''
     default_auto_field = "django.db.models.BigAutoField"
     name = "spotify_data"
