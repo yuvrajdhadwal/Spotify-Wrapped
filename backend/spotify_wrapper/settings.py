a56808755d1651f3ef0007434ef694bd23bed307
Yixiong Hao <yixiong_hao@outlook.com>
2024-11-18 16:23:58 -0500

    trying out new middleware

--- backend/spotify_wrapper/settings.py
+++ backend/spotify_wrapper/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'backend.middleware.CorsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
